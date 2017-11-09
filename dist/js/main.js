dab2ec6cf2f279f88e4533c4bfa3fcf5da57afb2
Andrew Toups <mistertoups@gmail.com>
2017-11-08 20:24:14 -0600

    First commit

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+// $('.splash .greeting').on('click', function() {
+//   $('.splash').toggleClass('nav');
+// })
+"use strict";
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+ // $('.splash .greeting').on('click', function() {
+ //   $('.splash').toggleClass('nav');
+ // })
