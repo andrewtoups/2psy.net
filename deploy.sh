e3f5bafa22c7da1c438cc60e793ec028afe8ed0d
Andrew Toups <mistertoups@gmail.com>
2020-12-09 22:08:18 +0000

    Removes npm-debug and ancient deploy script.

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "deploying to 2psy.net..."
-rsync -avh ~/Documents/Projects/2psy.net/ website:/var/www/html --exclude=src/ --exclude=node_modules/ --exclude=.gitignore --exclude=deploy.sh --exclude=Gruntfile.js --exclude=package.json --exclude=.DS_Store
-echo "done :)"
