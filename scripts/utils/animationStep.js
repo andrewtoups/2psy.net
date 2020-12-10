bc86c81f764f9df7cfcdfdda3f52d9b0e9a18d3e
Andrew Toups <mistertoups@gmail.com>
2020-12-10 01:35:25 +0000

    Cleans up code and adds notation.

--- scripts/utils/animationStep.js
+++ scripts/utils/animationStep.js
@@ -1,24 +1,30 @@
 define(['ko'], function(ko){
     var calculateAnimationTime = function(element, props){
         var duration, delay;
+
         if (props.action === 'add'){
+            //clone element to look ahead:
             var clone = element.cloneNode(true);
             clone.classList.add(props.class);
             clone.style.display = 'none';
             element.parentElement.appendChild(clone);
+
             duration = window.getComputedStyle(clone)["transition-duration"];
             delay = window.getComputedStyle(clone)["transition-delay"];
+            
             element.parentElement.removeChild(clone);
         } else {
             duration = window.getComputedStyle(element)["transition-duration"];
             delay = window.getComputedStyle(element)["transition-delay"];
         }
+
         duration = duration.indexOf(', ') != -1 ? duration.split(', ').map(n => parseFloat(n)) : duration;
         delay = delay.indexOf(', ') != -1 ? delay.split(', ').map(n => parseFloat(n)) : delay;
         duration = Array.isArray(duration) ? duration.reduce((n, m) => Math.max(n, m)) : parseFloat(duration);
         delay = Array.isArray(delay) ? delay.reduce((n, m) => Math.max(n, m)) : parseFloat(delay);
         duration *= 1000;
         delay *= 1000;
+
         return delay + duration;
     };
     return {
