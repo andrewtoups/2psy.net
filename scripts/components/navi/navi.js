39cd56e7c79ad4f7171acadd9701bea93ade4aee
Andrew Toups <mistertoups@gmail.com>
2020-12-11 08:45:57 +0000

    Refactors nav component to use state-based triggers.

--- scripts/components/navi/navi.js
+++ scripts/components/navi/navi.js
@@ -9,23 +9,32 @@ define([
         var self = this;
 
         self.isActive = ko.observable(false);
-        self.isActive.subscribe((newValue) => { vm.activeNav(newValue) });
         self.isAwake = ko.observable(false);
+        vm.currentRoute.subscribe(newValue => {
+            if (vm.contentPages.some(page => newValue === page.url)) {
+                if (!self.isAwake()) self.isAwake(true);
+                else self.isActive(true);
+            } else {
+                if (self.isActive(false));
+            }
+        });
+        self.isAwake.subscribe((newValue) => { vm.awakeNav(newValue) });
+        self.isActive.subscribe((newValue) => { vm.activeNav(newValue) });
 
         self.nav = [{
+            when: {state: self.isAwake, is: true},
             action: 'add',
             class: 'active'
         }];
-        self.page = ko.computed(() => vm.splash.pageEl());
-        self.animation = {
+        self.page = ko.computed(() => {
+            let page = vm.splash.pageEl();
+            page[0].when = {state: self.isActive, is: true};
+            return page;
+        });
+        self.transitions = {
             if: function() { return !self.isAwake() },
-            steps: [self.nav, self.page()],
-            onClick: () => {
-                self.isAwake(true);
-            },
-            complete: () => {
-                self.isActive(true);
-            }
+            steps: [self.nav, self.page()]
         };
+        transitions(self.transitions);
     };
 });
