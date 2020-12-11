6c0e8d93a488ed604b5747172553ddf3acb12c56
Andrew Toups <mistertoups@gmail.com>
2020-12-11 08:14:49 +0000

    Refactors animationTrigger to be standalone module instead of custom binding. Binding is no longer necessary because the transitions are triggered by state changes, which don't have to come from specific browser events or be tied to specific DOM elements.
    Breaks animationTrigger into multiple functions for easier readability.
    
    Renames function names to refer to transitions instead of animations.
    
    Refactors chain function to use a pre-calculated array of timeouts instead of calculating them on the fly.
    
    Refactors reverse mechanism to pre-calculate the reverse sequence and uses simplified logic to control animation direction.

--- scripts/utils/animationTrigger.js
+++ scripts/utils/animationTrigger.js
@@ -1,41 +1,137 @@
 define(['ko'], function(ko){
-    return {
-        init: function(element, valueAccessor, allBindings, data, context){
-            let thisValueAccessor = function(){
-                return function(){
-                    let options = ko.unwrap(valueAccessor());
-                    let cond = options.hasOwnProperty('if') ? options.if() : true;
-                    if (cond) {
-                        let steps = options.steps.flat();
-                        let complete = options.hasOwnProperty('complete') ? options.complete : false;
-                        let eachStep = options.hasOwnProperty('eachStep') ? options.eachStep : false;
-                        let onClick = options.hasOwnProperty('onClick') ? options.onClick : false;
-                        
-                        onClick && onClick(allBindings());
-                        counter3 = logCounter('animationTrigger onClick', counter3);
-                        
-                        let nextTimeout = 0;
-                        steps.forEach((step, index, array) => {
-                            let cList = Array.from(step.element.classList);
-                            if ((!cList.includes(step.class) && step.action === 'add') ||
-                            (cList.includes(step.class) && step.action === 'remove')) {
-                                nextTimeout += index === 0 ? 0 : array[index-1].timeout;
-                                setTimeout(function() {
-                                    counter2 = logCounter('animationTrigger timeout', counter2);
-                                    step.element.classList[step.action](step.class);
-                                    eachStep && eachStep(index, allBindings());
-                                    if (index + 1 === steps.length){
-                                        setTimeout(function(){
-                                            complete && complete(index, allBindings());
-                                        }, step.timeout); 
+    let transitions = function(options){
+        this.chainTransitions = () => {
+            let indexArr = [];
+            let timeoutArr = [];
+            
+            let chain = function(step, index, array){
+                if (options.rev) console.log(`${step.action} ${step.class} ${step.action === "add" ? "to" : "from"}: `, step.element, ` and wait for ${timeoutArr[index]}`);
+                if (indexArr.length < array.length) indexArr[index] = index;
+                let cList = Array.from(step.element.classList);
+                
+                if ((!cList.includes(step.class) && step.action === 'add') ||
+                (cList.includes(step.class) && step.action === 'remove')) {
+                    if (index === 0) {
+                        step.element.classList[step.action](step.class);
+                        options.eachStep && options.eachStep(indexArr[index]);
+                    } else {
+    
+                        setTimeout(function() {
+                            step.element.classList[step.action](step.class);
+                            options.eachStep && options.eachStep(indexArr[index]);
+                            
+                            if (index + 1 === indexArr.length){
+                                setTimeout(function(){
+                                    if (!options.rev && options.lastStep.when.state() !== options.lastStep.when.is) {
+                                        options.lastStep.when.state(options.lastStep.when.is);
+                                    } else if (options.rev && options.firstStep.when.state() === options.firstStep.when.is) {
+                                        options.firstStep.when.state(!options.firstStep.when.is);
                                     }
-                                }, nextTimeout);
+                                }, timeoutArr[index]);
                             }
-                        });
+                        }, timeoutArr[index - 1]);
                     }
-                };
+                    
+                }
+            };
+            
+            if (!options.rev) {
+                options.steps.forEach((step, index) => {
+                    indexArr[index] = index;
+                    timeoutArr[index] = index < 1 ? step.timeout :
+                                        index + 1 < options.steps.length ? step.timeout + timeoutArr[index - 1] :
+                                        step.timeout;
+                });
+
+                options.steps.forEach(chain);
+            } else {
+                options.revSteps.forEach((step, index) => {
+                    indexArr[index] = index;
+                    timeoutArr[index] = index < 1 ? step.timeout :
+                                        index + 1 < options.steps.length ? step.timeout + timeoutArr[index - 1] :
+                                        step.timeout;
+                });
+
+                indexArr.reverse();
+                options.revSteps.forEach(chain);
             }
-            ko.bindingHandlers.click.init(element, thisValueAccessor, allBindings, data, context);  
-        }
-    };
+        };
+
+        this.stateWrapper = (whenObj) => {
+            return (newState) => {
+                if (typeof newState === 'undefined') {
+                    return whenObj.state() === whenObj.is;
+                } else if (typeof newState === 'boolean') {
+                    whenObj.state(whenObj.is);
+                }
+            }
+        };
+
+        options.rev = false;
+
+        options.steps = options.steps.flat();
+        options.readySteps = ko.observableArray([]);
+        options.ready = ko.computed(() => options.readySteps().length === options.steps.length);
+
+        options.steps.forEach(step => {
+            if (!step.hasOwnProperty('ready')) step.ready = ko.observable(false);
+            else if (step.ready()) options.readySteps.push(step.ready());
+        });
+        if (!options.hasOwnProperty('if')) options.if = () => true;
+
+        options.steps.forEach(step => {
+            step.ready.subscribe(newValue => {
+               if (newValue) options.readySteps.push(newValue);
+            });
+        });
+        
+        options.ready.subscribe(newValue => {
+            if (newValue && options.if()) {
+                options.firstStep = options.steps[0];
+                options.lastStep = options.steps[options.steps.length - 1];
+                
+                if (options.firstStep.hasOwnProperty('when')) {
+                    options.firstStep.state = this.stateWrapper(options.firstStep.when); 
+                }
+                
+                if (options.lastStep.hasOwnProperty('when')) {
+                    options.lastStep.state = this.stateWrapper(options.lastStep.when);
+                }
+        
+                options.revSteps = [];
+                options.steps.forEach((step, index) => {
+                    options.revSteps[index] = {
+                        class: step.class,
+                        action: step.action === 'add' ? 'remove' : 'add',
+                        timeout: step.timeout,
+                        element: step.element
+                    };
+                    if (step.hasOwnProperty('when')) {
+                        options.revSteps[index].when = {
+                            state: step.when.state,
+                            is: step.when.is
+                        }
+                    }
+                });
+                options.revSteps.reverse();
+        
+                options.firstStep.when.state.subscribe(newValue => {
+                    if ((newValue === options.firstStep.when.is) &&
+                    (options.lastStep.when.state() !== options.lastStep.when.is)) {
+                        if (options.rev) options.rev = false;
+                        this.chainTransitions(options);
+                    }
+                });
+        
+                options.lastStep.when.state.subscribe(newValue => {
+                    if ((newValue !== options.lastStep.when.is) &&
+                    (options.firstStep.when.state() === options.firstStep.when.is)) {
+                        if (!options.rev) options.rev = true;
+                        this.chainTransitions(options);
+                    }
+                });
+            }
+        });
+    }
+    return transitions;
 });
