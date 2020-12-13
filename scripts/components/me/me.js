562b61ce6221084d193cb8ed5a1608c4e98ac5ef
Andrew Toups <mistertoups@gmail.com>
2020-12-13 05:27:47 +0000

    Loads contact links data for about me page.

--- scripts/components/me/me.js
+++ scripts/components/me/me.js
@@ -1,6 +1,11 @@
-define(['ko'], function(ko){
+define(['ko', 'utils/JSON.get'], function(ko){
     return function (){
         var self = this;
-        vm.me = self;
+        
+        self.contactLinks = ko.observableArray([]);
+        JSON.get('data/contactLinks.json', function(data){
+            if (Array.isArray(data)) self.contactLinks(data);
+            else console.log("Error loading contact links.")
+        });
     };
 });
