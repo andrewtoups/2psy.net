6fa4b333cca614c7c7eebc4159ed545640f3a069
Andrew Toups <mistertoups@gmail.com>
2020-12-13 05:34:59 +0000

    Adds template and loads data for music page.

--- scripts/components/music/music.js
+++ scripts/components/music/music.js
@@ -1,6 +1,15 @@
-define(['ko'], function(ko){
+define(['ko', 'utils/JSON.get'], function(ko){
     return function (){
         var self = this;
-        vm.music = self;
+
+        self.albums = ko.observableArray();
+        JSON.get('data/music.json', function(data){
+            if (Array.isArray(data) && data.length){
+                self.albums(data);
+                console.log(self.albums());
+            } else {
+                console.log("Error loading music data.");
+            }
+        });
     };
 });
